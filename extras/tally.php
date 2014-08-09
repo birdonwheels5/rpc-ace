24b9f1d9054bffbb35f73f3d582109466f80b86e
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-08 21:10:34 -0400

    Update tally.php

--- extras/tally.php
+++ extras/tally.php
@@ -15,10 +15,10 @@ if( !isset($argv[1]) )
 $numAddresses = 100;
 $txBufferSize = 150;
 
-$rpcUser = 'user';
+$rpcUser = 'birdonwheels5';
 $rpcPass = 'pass';
 $rpcHost = '127.0.0.1';
-$rpcPort = 12345;
+$rpcPort = 10889;
 
 require_once( 'easybitcoin.php' );
 $rpc = new Bitcoin( $rpcUser, $rpcPass, $rpcHost, $rpcPort );
@@ -64,9 +64,58 @@ $rpc = null;
 natsort( $addresses );
 $addresses = array_reverse( $addresses );
 
+file_put_contents( $argv[1],
+	"<html>
+	<head>
+		<meta charset=\"ISO-8859-1\">
+		<title>MYR Rich List</title>
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"https://birdonwheels5.no-ip.org/styles.css\" title=\"Default Styles\" media=\"screen\"/>
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"http://fonts.googleapis.com/css?family=Open+Sans\" title=\"Font Styles\"/>
+	</head>
+	
+	<body link=\"#E2E2E2\" vlink=\"#ADABAB\" style=\"background-image:url(https://birdonwheels5.no-ip.org/img/stars3.jpg);\">
+		<header>
+		
+				<div class=\"logoContainer\">
+					<img src=\"https://birdonwheel5.no-ip.org/img/logo.png\" alt=\"Myriadcoin Logo\" style=\"width:130%;\">
+				</div>
+
+				<div class=\"button\">
+					<p><a href =\"http://birdonwheels5.no-ip.org:3000\">Back to Block Explorer</a></p>
+				</div>
+				
+			</header>
+		<article style=\"color:#FFFFFF;\">
+				<p>
+					<center><img src=\"https://birdonwheel5.no-ip.org/logo.gif\"></center>
+					
+					<hr/>
+					<center><h1>Myriadcoin Rich List</h1></center>
+					<hr/>
+					<p>
+						<h2><u>Top 100 Richest Myriadcoin Addresses</u></h2><br/<br/>
+	
+						<p> " );
+
 $i = 0;
 while( (list($key, $value) = each( $addresses )) && $i++ < $numAddresses )
-    file_put_contents( $argv[1], "$key $value\n", FILE_APPEND );
+    file_put_contents( $argv[1], "$key $value" . " MYR\n" );
+	
+file_put_contents( $argv[1],
+	"</p>
+	<br/><br/>
+		</article>
+			
+			<div class=\"paddingBottom\">
+			</div>
+			
+			<footer>
+				2014 birdonwheels5.
+			</footer>
+		</div>
+	</body>
+	
+</html>" );
 
 echo "done! $txTotal transactions through " . count( $addresses ) . " unique addresses counted\n";
 
