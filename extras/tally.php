a3363bd0924fc6e0eb5b8ead1264ccbe8cefa3a9
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-09 22:42:10 -0400

    Update tally.php

--- extras/tally.php
+++ extras/tally.php
@@ -20,14 +20,44 @@ $rpcPass = 'pass';
 $rpcHost = '127.0.0.1';
 $rpcPort = 10889;
 
+$abort = false;
+$numAddresses = 1000;
+$txBufferSize = 150;
+$resume = "$rpcUser-$rpcPort-tally.dat";
+
+if( !isset($argv[1]) )
+    die( "Usage: php {$argv[0]} <output filename>\n" );
+
+function handleInt()
+{
+    global $abort;
+    $abort = true;
+}
+pcntl_signal( SIGINT, 'handleInt' );
+
 require_once( 'easybitcoin.php' );
 $rpc = new Bitcoin( $rpcUser, $rpcPass, $rpcHost, $rpcPort );
+$numBlocks = $rpc->getinfo()['blocks'];
+if( $rpc->status !== 200 && $rpc->error !== '' )
+    die( "Failed to connect. Check your coin's .conf file and your RPC parameters.\n" );
 
 $i = $txTotal = 0;
-$next = $rpc->getblockhash( 1 );
-$numBlocks = $rpc->getinfo()['blocks'];
+if( file_exists($resume) )
+{
+    $tally = unserialize( file_get_contents($resume) );
+    if( $tally['tally'] === true )
+    {
+        $i = $tally['last'];
+        $txTotal = $tally['txTotal'];
+        $addresses = $tally['addresses'];
+        $numAddresses = $tally['numAddresses'];
+        echo 'resuming from block ' . ( $i + 1 ) . ' - ';
+    }
+}
+
+$next = $rpc->getblockhash( $i + 1 );
 echo "$numBlocks blocks ... ";
-while( ++$i <= $numBlocks )
+while( ++$i <= $numBlocks && $abort === false )
 {
     if( $i % 1000 == 0 )
         echo "$i (tx# $txTotal)   ";
@@ -57,9 +87,19 @@ while( ++$i <= $numBlocks )
         if( count($txBuffer) > $txBufferSize )
             array_shift( $txBuffer );
     }
-    $next = $block['nextblockhash'];    
+    if( ($next = @$block['nextblockhash']) === null )
+        $abort = true;
+
+    pcntl_signal_dispatch();
 }
 $rpc = null;
+
+// save progress
+file_put_contents( $resume, serialize(['tally'=>true,
+                                       'numAddresses'=>$numAddresses,
+                                       'addresses'=>$addresses,
+                                       'txTotal'=>$txTotal,
+                                       'last'=>$i-1]) );
 
 natsort( $addresses );
 $addresses = array_reverse( $addresses );
@@ -69,15 +109,18 @@ file_put_contents( $argv[1],
 	<head>
 		<meta charset=\"ISO-8859-1\">
 		<title>MYR Rich List</title>
-		<link rel=\"stylesheet\" type=\"text/css\" href=\"https://birdonwheels5.no-ip.org/styles.css\" title=\"Default Styles\" media=\"screen\"/>
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"http://birdspool.no-ip.org:5567/static/styles.css\" title=\"Default Styles\" media=\"screen\"/>
 		<link rel=\"stylesheet\" type=\"text/css\" href=\"http://fonts.googleapis.com/css?family=Open+Sans\" title=\"Font Styles\"/>
 	</head>
 	
-	<body link=\"#E2E2E2\" vlink=\"#ADABAB\" style=\"background-image:url(https://birdonwheels5.no-ip.org/img/stars3.jpg);\">
+	<body link=\"#E2E2E2\" vlink=\"#ADABAB\" style=\"background-image:url(http://birdspool.no-ip.org:5567/static/img/stars3.jpg);\">
+	
+	<center><div class="container">
+	
 		<header>
 		
 				<div class=\"logoContainer\">
-					<img src=\"https://birdonwheel5.no-ip.org/img/logo.png\" alt=\"Myriadcoin Logo\" style=\"width:130%;\">
+					<img src=\"http://birdspool.no-ip.org:5567/static/img/logo.png\" alt=\"Myriadcoin Logo\" style=\"width:130%;\">
 				</div>
 
 				<div class=\"button\">
@@ -87,22 +130,22 @@ file_put_contents( $argv[1],
 			</header>
 		<article style=\"color:#FFFFFF;\">
 				<p>
-					<center><img src=\"https://birdonwheel5.no-ip.org/logo.gif\"></center>
+					<center><img src=\"http://birdspool.no-ip.org:5567/static/img/logo.gif\" style=\"width:10%\";></center>
 					
 					<hr/>
 					<center><h1>Myriadcoin Rich List (Compiled daily @ 1 AM EST)</h1></center>
 					<hr/>
 					<p>
-						<h2><u>Top 100 Richest Myriadcoin Addresses</u></h2><br/<br/>
+						<h2><u>Top 1000 Richest Myriadcoin Addresses</u></h2><br/<br/>
 	
-						<p> " );
+						<table style=\"width:500px\";> " );
 
 $i = 0;
 while( (list($key, $value) = each( $addresses )) && $i++ < $numAddresses )
-    file_put_contents( $argv[1], "$key $value" . " MYR\n" );
+    file_put_contents( $argv[1], "<tr><td>$i) </td> <td><a href=\"http://birdonwheels5.no-ip.org:3000/address/$key\">$key </a></td> <td> " . number_format($value, 2, '.', ',') . " </td <td> MYR</td></tr>\n", FILE_APPEND );
 	
 file_put_contents( $argv[1],
-	"</p>
+	"</table>
 	<br/><br/>
 		</article>
 			
@@ -117,6 +160,6 @@ file_put_contents( $argv[1],
 	
 </html>" );
 
-echo "done! $txTotal transactions through " . count( $addresses ) . " unique addresses counted\n";
+echo ( $abort ? 'aborted -' : 'done!' ) . " $txTotal transactions through " . count( $addresses ) . " unique addresses counted\n";
 
 ?>
